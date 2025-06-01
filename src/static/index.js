598c50c2778ce899536929f347829be9bd959418
Catherine <142168043+Catherine-99@users.noreply.github.com>
2025-06-01 20:50:24 +0100

    add animation for cat character

--- src/static/index.js
+++ src/static/index.js
@@ -19,5 +19,46 @@ function switchToLocation() {
 
 }
 
-goButton.addEventListener('click', switchToWeather)
-backButton.addEventListener('click', switchToLocation)
+goButton.addEventListener('click', switchToWeather);
+backButton.addEventListener('click', switchToLocation);
+
+//cat animation 
+const canvas = document.getElementById('cat-animation');
+const context = canvas.getContext('2d');
+const canvasWidth = canvas.width = 230;
+const canvasHeight = canvas.height = 230;
+
+const catImage = new Image();
+catImage.src = '/src/static/assets/catsprites.png';
+context.imageSmoothingEnabled = false;
+
+const spriteWidth = 64;
+const spriteHeight = 64;
+
+let frameX = 0;
+
+//frameY is which cat to display according to the weather 
+let frameY = 0;
+let frame = 0;
+const staggerFrames = 20;
+
+function animate(){
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    context.drawImage(catImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, canvasWidth, canvasHeight,);
+    if (frame % staggerFrames == 0){
+        if (frameX < 4) frameX++;
+        else frameX = 0;
+    }
+    frame++;
+    requestAnimationFrame(animate);
+}
+animate();
+
+
+//get weather data based on location 
+
+//get location input from user, send this to the backend, retrieve coordinates in th ebackend
+//then retrieve weather data and forecast in backend
+//send data to front end
+//display correct cat animation, weather icon, forecast icon and weekdays 
+//
